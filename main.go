dc24fa754f1ee83ffc23ffec697935404c68c409
k23dev <hashersmasher@proton.me>
2024-03-09 17:18:31 -0300

    add go-app

--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	auryDebug "aury/aury/aury_debug"
-	"aury/myplot/episode1"
+	"aury/screens"
+	"log"
+	"net/http"
+
+	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
 func main() {
-	ep1 := episode1.Episode()
-	// ep2 := episode2.Episode()
+	// ep1 := episode1.Episode()
+	// // ep2 := episode2.Episode()
 
-	// debug episode
-	auryDebug.Locations(&ep1.Locations)
-	auryDebug.Episode(&ep1)
-	// auryDebug.Episode(&ep2)
+	// // debug episode
+	// auryDebug.Locations(&ep1.Locations)
+	// auryDebug.Episode(&ep1)
+	// // auryDebug.Episode(&ep2)
 
-	// debug characters
-	// auryDebug.Character(&ep1.Characters[0])
-	auryDebug.Characters(&ep1.Characters)
-	// auryDebug.Characters(&ep2.Characters)
+	// // debug characters
+	// // auryDebug.Character(&ep1.Characters[0])
+	// auryDebug.Characters(&ep1.Characters)
+	// // auryDebug.Characters(&ep2.Characters)
+
+	app.Route("/", &screens.Hello{})
+
+	app.RunWhenOnBrowser()
+
+	http.Handle("/", &app.Handler{
+		Name:        "Hello",
+		Description: "An Hello World! example",
+	})
+
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
