ea7dddaa6263b93e0564fedda6241694b0c0c799
k23dev <hashersmasher@proton.me>
2024-03-09 01:32:19 -0300

    add default actions codes

---
+++ aury/milestonescodes/default.go
@@ -0,0 +1,27 @@
+package milestonescodes
+
+// generic
+
+const Generic uint = 0
+
+// Scene
+const SceneGeneric uint = 100
+const SetScene uint = 101
+
+// Location
+const LocationGeneric uint = 200
+const SetLocation uint = 201
+
+// dialog
+const DialogGeneric uint = 300
+const NewDialog uint = 301
+const DialogWithOptions uint = 302
+
+// characters
+const CharacterGeneric uint = 400
+const SetCharacter uint = 401
+
+// actions
+const ActionGeneric uint = 500
+const ActionOf uint = 501
+const ActionFromTo uint = 502
