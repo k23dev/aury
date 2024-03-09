164f8e361451438522fe67ae7adc18a8509f4c9d
k23dev <hashersmasher@proton.me>
2024-03-09 00:31:58 -0300

    add actions

--- aury/timeline/milestone.go
+++ aury/timeline/milestone.go
@@ -1,8 +1,13 @@
 package timeline
 
+import "aury/aury/characters"
+
 type Milestone struct {
 	Description string
 	ActionType  string
+	Action      interface{}
+	ActionFrom  characters.CharacterID
+	ActionTo    interface{}
 }
 
 func NewMilestone(description, actionType string) Milestone {
@@ -15,4 +20,45 @@ func NewMilestone(description, actionType string) Milestone {
 		Description: description,
 		ActionType:  actionType,
 	}
+}
+
+func NewMilestoneAction(description, actionType string) Milestone {
+
+	return Milestone{
+		Description: description,
+		ActionType:  actionType,
+	}
+}
+
+// set action maded by a character
+func NewMilestoneActionFrom(description, actionType string, characterFrom characters.CharacterID) Milestone {
+
+	return Milestone{
+		Description: description,
+		ActionType:  actionType,
+		ActionFrom:  characterFrom,
+	}
+}
+
+// set action maded by a character to [character,other]
+func NewMilestoneActionFromTo(description, actionType string, characterFrom characters.CharacterID, reciver interface{}) Milestone {
+
+	return Milestone{
+		Description: description,
+		ActionType:  actionType,
+		ActionFrom:  characterFrom,
+		ActionTo:    reciver,
+	}
+}
+
+func (m *Milestone) SetActionFrom(character characters.CharacterID) {
+	m.ActionFrom = character
+}
+
+func (m *Milestone) SetActionTo(reciver interface{}) {
+	m.ActionTo = reciver
+}
+
+func (m *Milestone) SetAction(action interface{}) {
+	m.Action = action
 }
--- aury/timeline/timeline.go
+++ aury/timeline/timeline.go
@@ -1,5 +1,7 @@
 package timeline
 
+import "aury/aury/characters"
+
 type Timeline struct {
 	Milestones []Milestone
 }
@@ -21,13 +23,25 @@ func (t *Timeline) AddMilestoneDialog(description string) {
 
 func (t *Timeline) AddMilestoneDialogWithOptions(description string) {
 	actionType := "_dialog_with_options"
-	newMilestone := NewMilestone(description, actionType)
+	newMilestone := NewMilestoneAction(description, actionType)
 	t.Milestones = append(t.Milestones, newMilestone)
 }
 
-func (t *Timeline) AddMilestoneAction(description string) {
+func (t *Timeline) AddMilestoneAction(description string, action interface{}) {
 	actionType := "_action"
-	newMilestone := NewMilestone(description, actionType)
+	newMilestone := NewMilestoneAction(description, actionType)
+	t.Milestones = append(t.Milestones, newMilestone)
+}
+
+func (t *Timeline) AddMilestoneActionOfCharacter(description string, character characters.CharacterID, action interface{}) {
+	actionType := "_actionOfCharacter"
+	newMilestone := NewMilestoneActionFrom(description, actionType, character)
+	t.Milestones = append(t.Milestones, newMilestone)
+}
+
+func (t *Timeline) AddMilestoneActionFromCharacterTo(description string, character characters.CharacterID, action interface{}, reciver interface{}) {
+	actionType := "_actionFromCharacterTo"
+	newMilestone := NewMilestoneActionFromTo(description, actionType, character, reciver)
 	t.Milestones = append(t.Milestones, newMilestone)
 }
 
