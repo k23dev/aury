d9a705cf772d220329c232b28beb32a60363ebdc
k23dev <hashersmasher@proton.me>
2024-03-11 23:43:21 -0300

    fix configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { postcss } from 'tailwindcss'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(),postcss],
+  plugins: [svelte()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 })
