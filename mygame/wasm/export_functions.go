10ade348bf52c96a3a203ae77f26facb490e00fe
k23dev <hashersmasher@proton.me>
2024-03-11 22:53:24 -0300

    add basic docs

--- mygame/wasm/export_functions.go
+++ mygame/wasm/export_functions.go
@@ -4,26 +4,7 @@ package main
 
 import "fmt"
 
+// your functions comes here
 func Greet(name string) string {
 	return fmt.Sprintf("Hello ,%s from Aury", name)
-}
-
-func Sumar(a, b int) int {
-	return a + b
-}
-
-func DividirX3(a, b int16, c float32) int16 {
-	return a + b
-}
-
-func loguear() interface{} {
-	return nil
-}
-
-func Prueba(a, b, c, d int, e float32, z, x, w string) int {
-	return a + b
-}
-
-func GetPepe(a interface{}) interface{} {
-	return a
 }
